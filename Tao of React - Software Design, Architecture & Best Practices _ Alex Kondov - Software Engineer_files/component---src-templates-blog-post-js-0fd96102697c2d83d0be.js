c4864c510906bfe68f7260bdacc898858beb43df
Ryan <ryanzhou7@gmail.com>
2022-03-11 07:19:16 -0800

    Files

---
+++ Tao of React - Software Design, Architecture & Best Practices _ Alex Kondov - Software Engineer_files/component---src-templates-blog-post-js-0fd96102697c2d83d0be.js
@@ -0,0 +1,37 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+"use strict";(self.webpackChunkalex_kondov_blog=self.webpackChunkalex_kondov_blog||[]).push([[989],{1257:function(e,t,a){a.r(t),a.d(t,{default:function(){return d}});var r=a(1721),n=a(7294),o=(a(5444),a(5414)),l=(a(1496),a(7065),a(7765)),c=a(6179),m=a(5713);var i=function(){return n.createElement("div",{className:"newsletter-form"},n.createElement("h3",{style:{marginTop:0}},"Learn More about Architecture and Software Design"),n.createElement("p",null,"Each month I send out a newsletter. Each one comes with my short thoughts and musings on a particular topic together with resources that I've found useful and a quote to ponder. No ads or spam. Unsubscribe any time."),n.createElement("form",{action:"https://web.archive.org/web/20211228152838/https://alexkondov.us2.list-manage.com/subscribe/post?u=fca75767fa83fa3473478b4ca&id=495bb9068f",method:"post",name:"mc-embedded-subscribe-form",target:"_blank"},n.createElement("div",{className:"newsletter-form-input"},n.createElement("input",{type:"email",name:"EMAIL",placeholder:"Your Email Address",style:{padding:"5px 10px",borderRadius:3}}),n.createElement("input",{type:"hidden",value:"1",name:"embed"}),n.createElement("input",{type:"submit",value:"Subscribe"}))),n.createElement("p",null,"Not sure?"," ",n.createElement("a",{href:"https://web.archive.org/web/20211228152838/https://codephilosophy.co"},"View previous newsletters.")))};function s(e){var t=e.image;return n.createElement("a",{href:"https://web.archive.org/web/20211228152838/https://taoofreact.com"},n.createElement("div",{className:"book"},n.createElement("img",{style:{borderRadius:3},src:t}),n.createElement("div",null,n.createElement("h2",{style:{marginTop:10,fontWeight:400}},"Tao of React"),n.createElement("div",{className:"divider"}),n.createElement("p",null,"Learn proven practices about React application architecture, component design, testing and performance."),n.createElement("a",{href:"https://web.archive.org/web/20211228152838/https://taoofreact.com"},"Get the Book"))))}var p=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.Z)(t,e),t.prototype.render=function(){var e,t=this.props.data.markdownRemark,a=this.props.data.site.siteMetadata.title,r=t.frontmatter.featuredImage&&t.frontmatter.featuredImage.childImageSharp?t.frontmatter.featuredImage.childImageSharp.fluid.src:null,p=this.props.pageContext,d=(p.previous,p.next,"https://web.archive.org/web/20211228152838/https://alexkondov.com"),u=r||this.props.data.cover.childImageSharp.fluid.src;return n.createElement(l.Z,{location:this.props.location,title:a},n.createElement(o.Z,null,n.createElement("meta",{name:"viewport",content:"width=device-width, initial-scale=1.0"}),n.createElement("meta",{property:"og:title",content:t.frontmatter.title+" | "+a}),n.createElement("meta",{property:"og:description",content:t.excerpt}),n.createElement("meta",{property:"og:image",content:""+d+u}),n.createElement("meta",{property:"og:url",content:""+d+t.fields.slug}),n.createElement("meta",{name:"twitter:card",content:"summary_large_image"}),n.createElement("meta",{property:"og:type",content:"article"}),n.createElement("meta",{property:"og:locale",content:"en_US"}),n.createElement("link",{rel:"canonical",href:""+d+t.fields.slug})),n.createElement(c.Z,{title:t.frontmatter.title,description:t.excerpt}),t.frontmatter.articleImage?n.createElement("img",{className:"post-image",src:t.frontmatter.articleImage}):null,n.createElement("h1",{className:"post-title",style:{marginTop:0}},t.frontmatter.title),n.createElement("p",{style:Object.assign({},(0,m.bA)(-.2),{display:"block",marginBottom:(0,m.qZ)(1),marginTop:(0,m.qZ)(-1)})},t.frontmatter.date," ",n.createElement("span",{className:"meta-separator"},"•")," ",(e=t.html,Math.floor(e.split(" ").length/200))," minute read"),n.createElement("div",{className:"content",dangerouslySetInnerHTML:{__html:t.html}}),n.createElement(i,null),n.createElement(s,{image:this.props.data.bookCover.childImageSharp.fluid.src}),n.createElement("div",{style:{textAlign:"center",marginTop:30}},n.createElement("a",{href:"/articles"},"Read More Articles")))},t}(n.Component),d=p}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-0fd96102697c2d83d0be.js.map
+
+}
+/*
+     FILE ARCHIVED ON 15:28:38 Dec 28, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 15:15:42 Mar 11, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 69.034
+  exclusion.robots: 0.122
+  exclusion.robots.policy: 0.113
+  RedisCDXSource: 0.581
+  esindex: 0.008
+  LoadShardBlock: 50.701 (3)
+  PetaboxLoader3.datanode: 56.902 (4)
+  CDXLines.iter: 15.128 (3)
+  load_resource: 499.677
+  PetaboxLoader3.resolve: 486.114
+*/
